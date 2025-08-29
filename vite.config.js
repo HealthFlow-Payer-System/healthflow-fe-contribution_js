cfd2bfc743629cfa47d64ea2b20b1121af72d6bf
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-08-29 23:27:17 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,24 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
 
+const noExternal = [
+  '@mui/material',
+  '@mui/system',
+  '@mui/icons-material',
+  '@mui/styled-engine',
+  '@emotion/react',
+  '@emotion/styled',
+  '@emotion/cache'
+];
+
 export default defineConfig({
   plugins: [
     react({
       jsxRuntime: 'automatic',
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
     }),
     svgr()
   ],
@@ -23,6 +37,8 @@ export default defineConfig({
       external: [
         'react',
         'react-dom',
+        'react/jsx-runtime',
+        'react/jsx-dev-runtime',
         'redux',
         'redux-thunk',
         'redux-api-middleware',
@@ -44,28 +60,64 @@ export default defineConfig({
         'react-router',
         'react-router-dom',
         'history',
+        '@emotion/react',
+        '@emotion/styled',
+        '@emotion/cache',
         '@mui/material',
         '@mui/icons-material',
-      
+        '@mui/system',
+        '@mui/styles',
         '@mui/material/styles',
         '@date-io/core',
         '@date-io/moment',
         'zxcvbn',
-     
+        'flat',
         /^@babel-.*/,
+        /^@date-io\/.*/,
         /^@openimis.*/
       ],
       output: {
         globals: {
-          'react': 'React',
-          'react-dom': 'ReactDOM'
-        }
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
+          'react/jsx-dev-runtime': 'jsxDevRuntime',
+          '@emotion/react': 'EmotionReact',
+          '@emotion/styled': 'EmotionStyled',
+          '@mui/material': 'MuiMaterial',
+        },
       }
     }
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
+      '@': resolve(__dirname, 'src'),
+      '@emotion/react': resolve(
+        __dirname,
+        'node_modules/@emotion/react'
+      ),
+      '@emotion/styled': resolve(
+        __dirname,
+        'node_modules/@emotion/styled'
+      ),
+      '@emotion/cache': resolve(
+        __dirname,
+        'node_modules/@emotion/cache'
+      ),
+    },
+  },
+  optimizeDeps: {
+    include: [
+      '@emotion/react',
+      '@emotion/styled',
+      '@emotion/cache',
+      '@mui/material',
+      '@mui/icons-material',
+      '@mui/system',
+    ],
+    force: true,
+  },
+  ssr: {
+    noExternal,
   }
 });
